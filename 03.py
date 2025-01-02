f56d2882b1bd2c059d798ee3ead11a62e84cc2ee
Samuel Hoover <hoovesam@oregonstate.edu>
2025-01-02 17:46:05 -0600

    day 03 part 1 solution

--- 03.py
+++ 03.py
@@ -1,3 +1,13 @@
+import re
+
 with open('03_input.txt') as f:
     data = f.read()
 
+mults = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', data)
+print(mults)
+
+sum = 0
+for x, y in mults:
+    sum += int(x) * int(y)
+
+print(sum)
